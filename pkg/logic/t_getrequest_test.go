09683abf3f3dc614cabf6628785293e4af935f22
Tudor Hulban <tudi@J3710>
2020-09-30 15:17:43 +0300

    sync work

--- pkg/logic/t_getrequest_test.go
+++ pkg/logic/t_getrequest_test.go
@@ -4,11 +4,12 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestMakeGETRequest(t *testing.T) {
-	code, errGET := makeGETRequest(url0)
-	require.Nil(t, errGET)
-	assert.GreaterOrEqual(t, code, 0)
+	_, errGET := makeGETRequest(url1)
+
+	// TODO: add verification according to time period
+	assert.Error(t, errGET)
+
 }
