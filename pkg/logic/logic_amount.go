ee4f4407f6d9748a23aee12f1a1538173a494669
Tudor Hulban <tudi@J3710>
2020-09-30 12:29:56 +0300

    added amount logic as ddl

--- pkg/logic/logic_amount.go
+++ pkg/logic/logic_amount.go
@@ -1,1 +1,19 @@
 package logic
+
+import (
+	"time"
+)
+
+func DecisionAmount(amount uint64) bool {
+	if amount < uint64(5000) {
+		time.Sleep(time.Duration(2) * time.Second)
+		return true
+	}
+
+	if amount >= 5000 && amount <= 10000 {
+		return false
+	}
+
+	time.Sleep(time.Duration(3) * time.Second)
+	return false
+}
