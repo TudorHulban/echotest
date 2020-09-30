f472253b74d392ab33dc554e8180e85a866ae0bc
Tudor Hulban <tudi@J3710>
2020-09-30 11:20:01 +0300

    added handlers

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+
+	e.POST("/api/decisions", handlerDecisions)
+	e.GET("/api/decisions", handlerDecisionsInDB)
+}
+
+// handlerDecisions ...
+func handlerDecisions(c echo.Context) error {
+	return c.String(http.StatusOK, "Aloha, World!")
+}
+
+// handlerDecisionsInDB ...
+func handlerDecisionsInDB(c echo.Context) error {
+	return c.String(http.StatusOK, "Aloha, World!")
 }
