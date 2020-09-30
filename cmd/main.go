f535fbd9d8027bdc5dd25b7d9b260ceb16dee1ac
David <davidbolet@gmail.com>
2020-09-30 17:23:39 +0200

    merge

--- cmd/main.go
+++ cmd/main.go
@@ -1,36 +1,53 @@
 package main
 
 import (
-	"context"
+	"log"
 	"net/http"
 
 	"github.com/TudorHulban/echotest/pkg/logic"
-	"github.com/TudorHulban/echotest/pkg/models"
 	"github.com/TudorHulban/echotest/pkg/repository"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 const (
-	url = "/api/decisions"
+	url         = "/api/decisions"
+	mongoURL    = "mongodb://localhost:27017"
+	mongoDBName = "decisions"
 )
 
+// RequestDecision Used to bind JSON body in handler.
+type RequestDecision struct {
+	Name   string
+	Amount int
+}
+
 func main() {
+	config := &repository.DBConfig{DatabaseName: mongoDBName, DBUrl: mongoURL}
+	helper, errRepo := repository.NewClient(config)
+	if errRepo != nil {
+		log.Fatal(errRepo.Error())
+	}
+
+	errCo := helper.Connect()
+	if errCo != nil {
+		log.Fatal("Cound not connect to mongo {} ", errCo.Error())
+	}
+
+	dbHandler := repository.NewDatabase(config, helper)
+	dbHandler.Client().StartSession()
+
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	e.Use(middleware.RequestID())
 	e.HideBanner = true
 
-	addRoutes(e)
-	e.Logger.Fatal(e.Start(":1323"))
-}
-
-func addRoutes(e *echo.Echo) {
 	e.POST(url, handlerDecisions)
-	e.GET(url, handlerDecisionsInDB) // requirement not in line with CRUD
+	e.GET(url, handlerDecisionsInDB)
+
+	e.Logger.Fatal(e.Start(":1323"))
 }
 
 // handlerDecisions Serving POST requests.
@@ -38,9 +55,9 @@ func addRoutes(e *echo.Echo) {
 // Manual test:
 // curl -X POST http://localhost:1323/api/decisions  -H 'Content-Type: application/json' -d '{"name":"X","amount":100}'
 func handlerDecisions(c echo.Context) error {
-	model := new(models.Decision)
+	model := new(RequestDecision)
 
-	// TODO: add input validation
+	// TODO: add validation
 
 	if errBind := c.Bind(model); errBind != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"status": errBind.Error()})
@@ -54,22 +71,15 @@ func handlerDecisions(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]bool{"decision": decision})
 }
 
-// handlerDecisionsInDB Saves decision to database.
-//
-// Manual test:
-// curl -X POST http://localhost:1323/api/decisions  -H 'Content-Type: application/json' -d '{"requestid":"100","name": "x", "amount":100, "answer": true}'
+// handlerDecisionsInDB ...
 func handlerDecisionsInDB(c echo.Context) error {
-	model := new(models.Decision)
+	model := new(RequestDecision)
 
 	if errBind := c.Bind(model); errBind != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"status": errBind.Error()})
 	}
 
-	// TODO: add authentication
-	// TODO: add request ID and decision bool
-	if errInsert := repository.GetInstance().Create(context.Background(), &models.Decision{Name: model.Name, Amount: model.Amount}); errInsert != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"status": errInsert.Error()})
-	}
+	// TODO: model is populated , can be saved in mongo
 
-	return c.String(http.StatusOK, "")
+	return c.String(http.StatusOK, "Aloha, World!")
 }
