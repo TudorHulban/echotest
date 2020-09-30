332734a57c159a40448360918c378089587aa222
Tudor Hulban <tudi@J3710>
2020-09-30 16:03:24 +0300

    enhanced inserter to db handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"log"
+	"context"
 	"net/http"
 
 	"github.com/TudorHulban/echotest/pkg/logic"
+	"github.com/TudorHulban/echotest/pkg/models"
 	"github.com/TudorHulban/echotest/pkg/repository"
 
 	"github.com/labstack/echo"
@@ -12,9 +13,7 @@ import (
 )
 
 const (
-	url         = "/api/decisions"
-	mongoURL    = "mongodb://localhost:27017"
-	mongoDBName = "decisions"
+	url = "/api/decisions"
 )
 
 // RequestDecision Used to bind JSON body in handler.
@@ -24,36 +23,20 @@ type RequestDecision struct {
 }
 
 func main() {
-	config := &repository.DBConfig{DatabaseName: mongoDBName, DBUrl: mongoURL}
-	helper, errRepo := repository.NewClient(config)
-	if errRepo != nil {
-		log.Fatal(errRepo.Error())
-	}
-
-	errCo := helper.Connect()
-	if errCo != nil {
-		log.Fatal("Cound not connect to mongo {} ", errCo.Error())
-	}
-
-	dbHandler := repository.NewDatabase(config, helper)
-	dbHandler.Client().StartSession()
-
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.RequestID())
-
 	e.HideBanner = true
 
 	addRoutes(e)
-
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func addRoutes(e *echo.Echo) {
 	e.POST(url, handlerDecisions)
-	e.GET(url, handlerDecisionsInDB)
+	e.GET(url, handlerDecisionsInDB) // requirement not in line with CRUD
 }
 
 // handlerDecisions Serving POST requests.
@@ -77,7 +60,7 @@ func handlerDecisions(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]bool{"decision": decision})
 }
 
-// handlerDecisionsInDB ...
+// handlerDecisionsInDB Saves decision to database.
 func handlerDecisionsInDB(c echo.Context) error {
 	model := new(RequestDecision)
 
@@ -85,7 +68,10 @@ func handlerDecisionsInDB(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, map[string]string{"status": errBind.Error()})
 	}
 
-	// TODO: model is populated , can be saved in mongo
+	// TODO: add request ID
+	if errInsert := repository.GetInstance().Create(context.Background(), &models.Decision{Name: model.Name, Amount: model.Amount}); errInsert != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"status": errInsert.Error()})
+	}
 
-	return c.String(http.StatusOK, "Aloha, World!")
+	return c.String(http.StatusOK, "")
 }
